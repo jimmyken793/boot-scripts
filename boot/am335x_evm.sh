7c13b398ca963b01042972f2cc5c0a06bdbae49f
Jimmy Cheng <jimmyken793@gmail.com>
2014-02-25 17:11:43 +0800

    fix possible corrupt net initialization script

--- boot/am335x_evm.sh
+++ boot/am335x_evm.sh
@@ -95,21 +95,28 @@ fi
 /sbin/ifconfig usb0 192.168.7.2 netmask 255.255.255.252
 /usr/sbin/udhcpd -S /etc/udhcpd.conf
 
-eth0_addr=$(ip addr list eth0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1)
-usb0_addr=$(ip addr list usb0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1)
-wlan0_addr=$(ip addr list wlan0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1)
-
 sed -i -e '/Address/d' /etc/issue
 
-if [ ! "x${eth0_addr}" = "x" ] ; then
-	echo "The IP Address for eth0 is: ${eth0_addr}" >> /etc/issue
+if [ -f /sys/class/net/eth0 ] ; then
+	eth0_addr=$(ip addr list eth0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1)
+	if [ ! "x${eth0_addr}" = "x" ] ; then
+		echo "The IP Address for eth0 is: ${eth0_addr}" >> /etc/issue
+	fi
 fi
-if [ ! "x${wlan0_addr}" = "x" ] ; then
-	echo "The IP Address for wlan0 is: ${wlan0_addr}" >> /etc/issue
+if [ -f /sys/class/net/usb0 ] ; then
+	usb0_addr=$(ip addr list usb0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1)
+	if [ ! "x${usb0_addr}" = "x" ] ; then
+		echo "The IP Address for usb0 is: ${usb0_addr}" >> /etc/issue
+	fi
 fi
-if [ ! "x${usb0_addr}" = "x" ] ; then
-	echo "The IP Address for usb0 is: ${usb0_addr}" >> /etc/issue
+if [ -f /sys/class/net/wlan0 ] ; then
+	wlan0_addr=$(ip addr list wlan0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1)
+	if [ ! "x${wlan0_addr}" = "x" ] ; then
+		echo "The IP Address for wlan0 is: ${wlan0_addr}" >> /etc/issue
+	fi
 fi
+
+
 
 if [ -f /boot/uboot/flash-eMMC.txt ] ; then
 	if [ ! -d /boot/uboot/debug/ ] ; then
@@ -122,7 +129,7 @@ if [ -f /boot/uboot/flash-eMMC.txt ] ; then
 fi
 
 if [ -f /boot/uboot/resizerootfs ] || [ -f /resizerootfs ] ; then
-	if [ ! -d /boot/uboot/debug/ ] ; then
+	if [ ! -d /boot/uboot/debug/ ] ; thenx
 		mkdir -p /boot/uboot/debug/ || true
 	fi
 
