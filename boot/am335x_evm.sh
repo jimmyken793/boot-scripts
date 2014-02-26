ba29faae779362cf27fa20838578a2baf48df8ca
Jimmy Cheng <jimmyken793@gmail.com>
2014-02-27 01:23:42 +0800

    fix network interface condition

--- boot/am335x_evm.sh
+++ boot/am335x_evm.sh
@@ -73,6 +73,7 @@ else
 fi
 
 modprobe g_multi file=${gadget_partition} cdrom=0 stall=0 removable=1 nofua=1 iSerialNumber=${SERIAL_NUMBER} iManufacturer=Circuitco  iProduct=BeagleBone${BLACK} host_addr=${cpsw_1_mac}
+sudo modprobe g_multi cdrom=0 stall=0 removable=1 nofua=1 iSerialNumber=1234534 iManufacturer=Circuitco  iProduct=BeagleBone host_addr=C8:A0:30:A0:DD:68
 
 sleep 1
 
@@ -97,19 +98,19 @@ fi
 
 sed -i -e '/Address/d' /etc/issue
 
-if [ -f /sys/class/net/eth0 ] ; then
+if [ -d /sys/class/net/eth0 ] ; then
 	eth0_addr=$(ip addr list eth0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1)
 	if [ ! "x${eth0_addr}" = "x" ] ; then
 		echo "The IP Address for eth0 is: ${eth0_addr}" >> /etc/issue
 	fi
 fi
-if [ -f /sys/class/net/usb0 ] ; then
+if [ -d /sys/class/net/usb0 ] ; then
 	usb0_addr=$(ip addr list usb0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1)
 	if [ ! "x${usb0_addr}" = "x" ] ; then
 		echo "The IP Address for usb0 is: ${usb0_addr}" >> /etc/issue
 	fi
 fi
-if [ -f /sys/class/net/wlan0 ] ; then
+if [ -d /sys/class/net/wlan0 ] ; then
 	wlan0_addr=$(ip addr list wlan0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1)
 	if [ ! "x${wlan0_addr}" = "x" ] ; then
 		echo "The IP Address for wlan0 is: ${wlan0_addr}" >> /etc/issue
@@ -129,7 +130,7 @@ if [ -f /boot/uboot/flash-eMMC.txt ] ; then
 fi
 
 if [ -f /boot/uboot/resizerootfs ] || [ -f /resizerootfs ] ; then
-	if [ ! -d /boot/uboot/debug/ ] ; thenx
+	if [ ! -d /boot/uboot/debug/ ] ; then
 		mkdir -p /boot/uboot/debug/ || true
 	fi
 
